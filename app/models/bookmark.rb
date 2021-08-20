b3b2ea1764c6c9d9c98d5d94650583e942990e73
Ben West <benwest0291@outlook.com>
2021-08-20 12:13:24 +0100

    used sams code to finish backend

---
+++ app/models/bookmark.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+
+  belongs_to :list
+  validates :movie_id, presence: true, uniqueness: { scope: :list_id }
+  validates :list_id, presence: true
+  validates :comment, length: { minimum: 6 }
+end
